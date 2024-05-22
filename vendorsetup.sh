ce074d75786182ebf03acf6017453568ecf587b1
Darth9 <reyprimerjefe@gmail.com>
2024-05-22 20:37:09 +0100

    The updated magiskboot is fine here.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -50,6 +50,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export FOX_USE_NANO_EDITOR=1
 	export FOX_DELETE_AROMAFM=1
 	export FOX_DELETE_INITD_ADDON=1
+	export FOX_USE_UPDATED_MAGISKBOOT=1
 else
 	if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
 		echo "I: This script requires bash. Not processing the $FDEVICE $(basename $0)"
