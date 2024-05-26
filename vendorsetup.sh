1b00f06d4492b7a98624f8bf628af44519e173a5
Darth9 <reyprimerjefe@gmail.com>
2024-05-26 19:59:01 +0100

    Add zstd

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -50,6 +50,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export FOX_DELETE_AROMAFM=1
 	export FOX_DELETE_INITD_ADDON=1
 	export FOX_USE_UPDATED_MAGISKBOOT=1
+	export FOX_USE_ZSTD_BINARY=1
 else
 	if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
 		echo "I: This script requires bash. Not processing the $FDEVICE $(basename $0)"
